5b860d9c9bada35c21944f869aa45f6a9f5b4c36
Hashin Panakkaparambil <hashin.p@travelodge.co.uk>
2020-03-10 21:05:15 +0000

    removes todo repository

--- todo/src/Repository/UserRepository.php
+++ todo/src/Repository/UserRepository.php
@@ -20,35 +20,6 @@ class UserRepository extends ServiceEntityRepository  implements UserRepositoryI
         parent::__construct($registry, User::class);
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
-
     public function findByUsername(string $username): \Hash\Domain\Todo\User\User
     {
         $persistenceModel = $this->findOneBy([
