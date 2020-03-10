d78cc6210748d8e0b52d573864051691007e15f2
Hashin Panakkaparambil <hashin.p@travelodge.co.uk>
2020-03-10 20:24:12 +0000

    spec for delete todo usecase

---
+++ todo/spec/UseCase/DeleteTodoSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\UseCase;
+
+use Hash\Domain\Todo\User\User;
+use Hash\Domain\Todo\User\UserService;
+use PhpSpec\ObjectBehavior;
+
+/**
+ * @mixin DeleteTodo
+ */
+class DeleteTodoSpec extends ObjectBehavior
+{
+
+    function let(UserService $userService)
+    {
+        $this->beConstructedWith($userService);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(DeleteTodo::class);
+    }
+
+    function it_deletes(UserService $userService, User $hashin)
+    {
+        $username = 'hashin';
+        $todoId = 1;
+        $userService->getUser($username)->willReturn($hashin);
+        $hashin->deleteTodo($todoId)->shouldBeCalled();
+        $userService->saveUser($hashin)->shouldBeCalled();
+        $this->execute($username, $todoId);
+    }
+}
